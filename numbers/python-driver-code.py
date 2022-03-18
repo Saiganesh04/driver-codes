36d75719efbe542c432012c0e8dc8a66e9a8ffb4
Saiganesh04 <73642194+Saiganesh04@users.noreply.github.com>
2022-03-18 13:01:52 +0530

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -1,13 +1,40 @@
-# Python Driver Code
-
+import math
 def solve(n: int) -> str:
-  # Your code goes here
-  # n is the given input
-  return "Special"
+    n = int(n)
+    ls = []
+    while n % 2 == 0:
+        ls.append(2)
+        n = n / 2
 
-# The following snippet reads the input in the required format. 
-# Just complete the solve function above. 
+    for i in range(3, int(math.sqrt(n))+1, 2):
+        while n % i == 0:
+            ls.append(i)
+            n = n / i
 
+    if n > 2:
+        ls.append(n)
+       
+    s = set(ls)
+    if len(s)>2:
+        return 'Regular'
+       
+    else:
+        if len(s)==1:
+            if 2 in s or 7 in s:
+                return 'Special'
+            else:
+                return 'Regular'
+        else:
+            if 2 in s:
+                if 7 in s:
+                    return 'Special'
+            elif 7 in s:
+                if 2 in s:
+                    return 'Special'
+            else:
+                return 'Regular'
+
+    return 'Regular'
 T = int(input())
 for i in range(T):
   test_case = input()
